e083d1828053c7a112f11b5ab7e22de3d38373d1
Tekiter <tekiter8@gmail.com>
2023-01-13 23:47:27 +0900

    feat: 공홈팀 서버 추가

--- src/checker.ts
+++ src/checker.ts
@@ -1,17 +1,38 @@
 export const checkerList: CheckerEntry[] = [
   {
-    name: "플레이그라운드 프로덕션",
+    name: "플레이그라운드 프로덕션 API",
     url: "https://playground.api.sopt.org",
     channel: "#makers-doctor",
     checker: checkPlaygroundAPIServer,
     mentions: "pg-all",
   },
   {
-    name: "플레이그라운드 개발용",
+    name: "플레이그라운드 개발용 API",
     url: "https://playground.dev.sopt.org",
     channel: "#makers-doctor",
     checker: checkPlaygroundAPIServer,
     mentions: "pg-all",
+  },
+  {
+    name: "공식홈페이지 프론트엔드",
+    url: "https://sopt.org",
+    channel: "#makers-doctor",
+    checker: check200Response,
+    mentions: "<@U0420HE8JLV>",
+  },
+  {
+    name: "공식홈페이지 프론트엔드",
+    url: "https://www.sopt.org",
+    channel: "#makers-doctor",
+    checker: check200Response,
+    mentions: "<@U0420HE8JLV>",
+  },
+  {
+    name: "공식홈페이지 API",
+    url: "https://api.sopt.org",
+    channel: "#makers-doctor",
+    checker: check200Response,
+    mentions: "<@U0420HE8JLV>",
   },
 ];
 
@@ -30,6 +51,16 @@ async function checkPlaygroundAPIServer(url: string) {
 
     return body.toLowerCase().includes("hello internal");
   } catch (e) {
+    return false;
+  }
+}
+
+async function check200Response(url: string) {
+  try {
+    const res = await fetch(url);
+
+    return res.status === 200;
+  } catch {
     return false;
   }
 }
